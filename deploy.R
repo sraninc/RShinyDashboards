531a7d81717d8c9a85c1db188a784c098624f84c
Sneha Rani <91280077+sraninc@users.noreply.github.com>
2024-12-05 16:43:37 -0800

    Update deploy.R

--- deploy.R
+++ deploy.R
@@ -1,7 +1,7 @@
 # Set up Shinyapps.io account for https
-rsconnect::setAccountInfo(name='sneharani', 
-                          token='CE8E29FA4226ED9F74B046896E6C2190', 
-                          secret='/jj/vqMzKCJN0hMM50OYgAP2Aq7a1Lq1+BnKr6Ny')
+rsconnect::setAccountInfo(name='[Add your own name here]', 
+                          token='[Add your own token here]', 
+                          secret='[Add your own token here]')
 
 # Deploy the app to Shinyapps.io
-rsconnect::deployApp('C:/Users/sne17/Desktop/Lenovo/DataTrade3/')
+rsconnect::deployApp('address/')
